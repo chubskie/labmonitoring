c57735fe37349d8ac8373c54c027de323ddab4af
Marjon <ramosmarjonf@gmail.com>
2020-10-09 08:20:47 +0800

    V0.6.1 - added forms on blade file

--- resources/views/accounts.blade.php
+++ resources/views/accounts.blade.php
@@ -1,5 +1,70 @@
 @extends('_layout')
 
 @section('body')
-Accounts
+    <div class="card">
+        <header class="card-header">
+            <p class="card-header-title">
+                Admin Setting
+            </p>
+        </header>
+        <div class="card-content">
+            <div class="content">
+                <div class="field">
+                    <label class="label">Username</label>
+                    <div class="control">
+                        <input class="input" type="text">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Current Password</label>
+                    <div class="control">
+                        <input class="input" type="password">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">New Password</label>
+                    <div class="control">
+                        <input class="input" type="password">
+                    </div>
+                </div>
+                <div class="control">
+                    <button class="button is-primary">Submit</button>
+                </div>
+            </div>
+        </div>
+    
+    </div>
+    <br>
+    <div class="card">
+        <header class="card-header">
+            <p class="card-header-title">
+                Add Account
+            </p>
+        </header>
+        <div class="card-content">
+            <div class="content">
+                <div class="field">
+                    <label class="label">Username</label>
+                    <div class="control">
+                        <input class="input" type="text">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Email</label>
+                    <div class="control">
+                        <input class="input" type="email">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Password</label>
+                    <div class="control">
+                        <input class="input" type="passowrd">
+                    </div>
+                </div>
+                <div class="control">
+                    <button class="button is-primary">Submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
