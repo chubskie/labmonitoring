7a27441fad780b49686638bc87ec5fe9996a949b
Marjon <ramosmarjonf@gmail.com>
2020-10-09 12:34:10 +0800

    V0.6.3 - Added table / Remove max-width/height and use columns in layout file

--- resources/views/_layout.blade.php
+++ resources/views/_layout.blade.php
@@ -1,20 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="csrf-token" content="{{ csrf_token() }}">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Lab Monitoring</title>
-	@include('_styles')
-	@yield('styles')
+    <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab Monitoring</title>
+    @include('_styles')
+    @yield('styles')
 </head>
+
 <body>
-	@include('_sidebar')
-	<div id="body">
-		@yield('body')
-	</div>
-	
-	@include('_scripts')
-	@yield('scripts')
+    @include('_sidebar')
+    <div id="body">
+        <div class="columns is-centered">
+            <div class="column">
+                <section class="section">
+                    <div class="container">
+
+						@yield('body')
+						
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+
+    @include('_scripts')
+    @yield('scripts')
 </body>
+
 </html>
--- resources/views/accounts.blade.php
+++ resources/views/accounts.blade.php
@@ -1,70 +1,96 @@
 @extends('_layout')
 
 @section('body')
-    <div class="card">
-        <header class="card-header">
-            <p class="card-header-title">
-                Admin Setting
-            </p>
-        </header>
-        <div class="card-content">
-            <div class="content">
-                <div class="field">
-                    <label class="label">Username</label>
-                    <div class="control">
-                        <input class="input" type="text">
-                    </div>
-                </div>
-                <div class="field">
-                    <label class="label">Current Password</label>
-                    <div class="control">
-                        <input class="input" type="password">
-                    </div>
-                </div>
-                <div class="field">
-                    <label class="label">New Password</label>
-                    <div class="control">
-                        <input class="input" type="password">
-                    </div>
-                </div>
+
+<div class="card">
+    <header class="card-header">
+        <p class="card-header-title">
+            Admin Setting
+        </p>
+    </header>
+    <div class="card-content">
+        <div class="content">
+            <div class="field">
+                <label class="label">Username</label>
                 <div class="control">
-                    <button class="button is-primary">Submit</button>
+                    <input class="input" type="text">
                 </div>
             </div>
-        </div>
-    
-    </div>
-    <br>
-    <div class="card">
-        <header class="card-header">
-            <p class="card-header-title">
-                Add Account
-            </p>
-        </header>
-        <div class="card-content">
-            <div class="content">
-                <div class="field">
-                    <label class="label">Username</label>
-                    <div class="control">
-                        <input class="input" type="text">
-                    </div>
-                </div>
-                <div class="field">
-                    <label class="label">Email</label>
-                    <div class="control">
-                        <input class="input" type="email">
-                    </div>
-                </div>
-                <div class="field">
-                    <label class="label">Password</label>
-                    <div class="control">
-                        <input class="input" type="passowrd">
-                    </div>
-                </div>
+            <div class="field">
+                <label class="label">Current Password</label>
                 <div class="control">
-                    <button class="button is-primary">Submit</button>
+                    <input class="input" type="password">
                 </div>
+            </div>
+            <div class="field">
+                <label class="label">New Password</label>
+                <div class="control">
+                    <input class="input" type="password">
+                </div>
+            </div>
+            <div class="control">
+                <button class="button is-primary">Submit</button>
             </div>
         </div>
     </div>
+</div>
+<br>
+<div class="card">
+    <header class="card-header">
+        <p class="card-header-title">
+            Add Account
+        </p>
+    </header>
+    <div class="card-content">
+        <div class="content">
+            <div class="field">
+                <label class="label">Username</label>
+                <div class="control">
+                    <input class="input" type="text">
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">Email</label>
+                <div class="control">
+                    <input class="input" type="email">
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">Password</label>
+                <div class="control">
+                    <input class="input" type="passowrd">
+                </div>
+            </div>
+            <div class="control">
+                <button class="button is-primary">Submit</button>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+
+<table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+                <button class="button">Edit</button>
+            </td>
+        </tr>
+
+    </tbody>
+</table>
+
+
 @endsection
