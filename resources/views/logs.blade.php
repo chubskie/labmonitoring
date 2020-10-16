225022725b5efc7b4c72d416035d36ff0de71706
nird931 <palenciaaldrin22@gmail.com>
2020-10-17 01:59:41 +0800

    Added Logs route

--- resources/views/logs.blade.php
+++ resources/views/logs.blade.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logs</title>
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
         <style>
@@ -62,32 +60,19 @@
                 margin-bottom: 30px;
             }
         </style>
-    </head>
-    <body>
+</head>
+<body>
+
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Aldrin Palencia
+    <div class="content">
+    <div class="title m-b-md">
+                    Logs Page
                 </div>
 
                 <div class="links">
-                    <a href="/logs">Click to view my Simple Page</a>
+                    <a href="/">Return</a>
                 </div>
-            </div>
-        </div>
-    </body>
+</div>
+</div>
+</body>
 </html>
