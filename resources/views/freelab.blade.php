92d9596fd4fb28a64cb9a1caba2031cc6d99c113
McPerry11 <mack.perry.co@gmail.com>
2020-11-08 00:03:35 +0800

    V0.7.2 - Modified freelab form. Removed failed jobs migration.

--- resources/views/freelab.blade.php
+++ resources/views/freelab.blade.php
@@ -3,7 +3,8 @@
 @section('body')
 <div class="columns is-centered">
 	<div class="column is-6">
-		<form class="box mt-4">
+		<form class="box mt-4" method="POST" action="{{ URL::to('freelab-store') }}">
+			@csrf
 			<h3 class="title has-text-centered">Free Lab Form</h3>
 		</form>
 	</div>
