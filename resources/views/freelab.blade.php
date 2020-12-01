8b727348c76bf01eac59da0a2438447e4a34a203
McPerry11 <mack.perry.co@gmail.com>
2020-12-01 22:12:55 +0800

    V0.11.1 - Modified free lab module

--- resources/views/freelab.blade.php
+++ resources/views/freelab.blade.php
@@ -1,81 +1,127 @@
 @extends('_layout')
 
+@section('styles')
+<style>
+	input[type="time"] {
+		width: 150px;
+	}
+</style>
+@endsection
+
 @section('body')
 <div class="columns is-centered">
 	<div class="column is-6">
 		<form class="box mt-4" method="POST" action="{{ URL::to('freelab-store') }}">
 			@csrf
 			<h3 class="title has-text-centered">Free Lab Form</h3>
-
-
-			<div class="field">
-				<label class="label">First Name</label>
-				<div class="control">
-					<input class="input" type="text" placeholder="">
+			<hr>
+			<div class="field is-horizontal">
+				<div class="field-label">
+					<label class="label">Full Name</label>
 				</div>
-			</div>
-			<div class="field">
-				<label class="label">Middle Initial</label>
-				<div class="control">
-					<input class="input" type="text" placeholder="">
-				</div>
-			</div>
-			<div class="field">
-				<label class="label">Last Name</label>
-				<div class="control">
-					<input class="input" type="text" placeholder="">
-				</div>
-			</div>
-			<div class="field">
-				<label class="label">Course</label>
-				<div class="control">
-					<input class="input" type="text" placeholder="">
-				</div>
-			</div>
-			<div class="field">
-				<label class="label">Student Number</label>
-				<div class="control">
-					<input class="input" type="number" placeholder="">
-				</div>
-			</div>
-
-			<div class="field">
-				<label class="label">Section</label>
-				<div class="control">
-					<div class="select">
-						<select>
-							<option>Select dropdown</option>
-							<option>With options</option>
-						</select>
+				<div class="field-body">
+					<div class="field is-grouped">
+						<div class="control is-expanded">
+							<input type="text" id="lastname" class="input" placeholder="Last Name" required>
+						</div>
+						<div class="control is-expanded">
+							<input type="text" id="firstname" class="input" placeholder="First Name">
+						</div>
+						<div class="control" style="width:70px">
+							<input type="text" id="middlename" class="input" placeholder="M.I.">
+						</div>
 					</div>
 				</div>
 			</div>
-			<div class="field is-grouped">
-				<div class="control">
+			<div class="field is-horizontal">
+				<div class="field-label">
+					<label class="label">Student Number</label>
+				</div>
+				<div class="field-body">
+					<div class="field">
+						<div class="control">
+							<input type="number" id="sn" class="input" placeholder="XXXXXXXXXXX" required>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="field is-horizontal">
+				<div class="field-label">
+					<label class="label">Course</label>
+				</div>
+				<div class="field-body">
+					<div class="field">
+						<div class="control is-expanded">
+							<div class="select">
+								<select id="course" required>
+									<option value="" selected disabled>Choose Course</option>
+									<option value="BSCS">BSCS</option>
+									<option value="BSIT">BSIT</option>
+									<option value="BSIS">BSIS</option>
+									<option value="BSEMC-GD">BSEMC-GD</option>
+									<option value="BSEMC-DA">BSEMC-DA</option>
+								</select>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="field is-horizontal">
+				<div class="field-label">
+					<label class="label">Section</label>
+				</div>
+				<div class="field-body">
+					<div class="field">
+						<div class="control">
+							<input type="text" id="section" class="input" required>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="field is-horizontal">
+				<div class="field-label">
+					<label class="label">Laboratory</label>
+				</div>
+				<div class="field-body">
+					<div class="field">
+						<div class="control">
+							<div class="select">
+								<select id="lab" required>
+									{{-- List options here --}}
+								</select>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="field is-horizontal">
+				<div class="field-label">
 					<label class="label">Time In</label>
-					<div class="control">
-						<input class="input" type="time">
+				</div>
+				<div class="field-body">
+					<div class="field">
+						<div class="control">
+							<input type="time" id="in" class="input" required>
+						</div>
 					</div>
 				</div>
-				<div class="control">
+			</div>
+			<div class="field is-horizontal">
+				<div class="field-label">
 					<label class="label">Time Out</label>
-					<div class="control">
-						<input class="input" type="time">
+				</div>
+				<div class="field-body">
+					<div class="field">
+						<div class="control">
+							<input type="time" id="out" class="input" required>
+						</div>
 					</div>
 				</div>
 			</div>
-
-
-			<div class="field is-grouped">
-				<div class="control">
-					<button class="button is-link">Submit</button>
-				</div>
-				<div class="control">
-					<a href="/" class="button is-link is-light">Cancel</a>
-				</div>
+			<div class="buttons is-centered pt-3">
+				<button class="button is-success">Submit</button>
+				<a href="{{ route('dashboard') }}" class="button is-danger is-outlined">Cancel</a>
 			</div>
-
-
 		</form>
 	</div>
 </div>
