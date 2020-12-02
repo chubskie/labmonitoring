5ca4744504c65221da0aa78350412630755df9d4
McPerry11 <mack.perry.co@gmail.com>
2020-12-03 00:55:02 +0800

    V0.12.5 - Fixed logs module

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -16,14 +16,14 @@ class IndexController extends Controller
 	}
 
 	public function logs(Request $request) {
-		if ($request->search == '') {
-			$logs = Log::orderBy('created_at', 'desc')->paginate(50);
-			return response()->json(['logs' => $logs]);
-		} else {
-			$logs = Log::orderBy('created_at', 'desc')->paginate(50);
-			return view('logs', [
-				'logs' => $logs
-			]);
-		}
+		// if ($request->search == '') {
+		// 	$logs = Log::orderBy('created_at', 'desc')->paginate(50);
+		// 	return response()->json(['logs' => $logs]);
+		// } else {
+		$logs = Log::orderBy('created_at', 'desc')->paginate(50);
+		return view('logs', [
+			'logs' => $logs
+		]);
+		// }
 	}
 }
