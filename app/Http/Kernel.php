d6577503287c30e05f85b30b1f2cd44f2b2069ca
Marjon <ramosmarjonf@gmail.com>
2020-12-10 05:39:56 +0800

    remove rate limitin api

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:10,3',
+            // 'throttle:10,3',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
